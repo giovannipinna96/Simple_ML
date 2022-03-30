a4191cc72b286bf855ac61167aa4f2801e81f8af
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-03-30 16:52:09 +0200

    modified the shuffle=True

--- Data.py
+++ Data.py
@@ -28,25 +28,29 @@ def load_housing_dataset():
     return housing
 
 
-def load_split_iris(test_size=0.3, shuffle=True):
+def load_split_iris(test_size=0.3, shuffle=False, random_state=1):
     X, y = load_iris_dataset()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle,
+                                                        random_state=random_state)
     return X_train, X_test, y_train, y_test
 
 
-def load_split_wine(test_size=0.3, shuffle=True):
+def load_split_wine(test_size=0.3, shuffle=False, random_state=1):
     X, y = load_wine_dataset()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle,
+                                                        random_state=random_state)
     return X_train, X_test, y_train, y_test
 
 
-def load_split_besast_cancer_wisconsin(test_size=0.3, shuffle=True):
+def load_split_besast_cancer_wisconsin(test_size=0.3, shuffle=False, random_state=1):
     X, y = load_besast_cancer_wisconsin_dataset()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle,
+                                                        random_state=random_state)
     return X_train, X_test, y_train, y_test
 
 
-def load_split_housing(test_size=0.3, shuffle=True):
+def load_split_housing(test_size=0.3, shuffle=False, random_state=1):
     X, y = load_housing_dataset()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=shuffle,
+                                                        random_state=random_state)
     return X_train, X_test, y_train, y_test
